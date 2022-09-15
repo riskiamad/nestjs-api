999c3d52f86e81ae93a61e22405ca464f2cac9bc
Achmad Rizky Syahrani <achmad.rizky@kodingworks.io>
2022-09-15 14:01:40 +0700

    initial commit

---
+++ src/bookmark/bookmark.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { GetUser } from '../auth/decorator';
+import { BookmarkService } from './bookmark.service';
+import { EditBookmarkDto } from './dto';
+import { CreateBookmarkDto } from './dto/create-bookmark.dto';
+import { JwtGuard } from '../auth/guard';
+
+@Controller('bookmark')
+export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+  @UseGuards(JwtGuard)
+  @Get()
+  getBookmarks(@GetUser('id') userId: number) {
+    return this.bookmarkService.getBookmarks(userId);
+  }
+
+  @Get(':id')
+  getBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+  }
+
+  @Post()
+  createBookmark(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateBookmarkDto,
+  ) {
+    return this.bookmarkService.createBookmark(userId, dto);
+  }
+
+  @Patch(':id')
+  editBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+    @Body() dto: EditBookmarkDto,
+  ) {
+    return this.bookmarkService.editBookmarkById(userId, bookmarkId, dto);
+  }
+
+  @Delete(':id')
+  deleteBookmark(
+    @GetUser('id') userId: number,
+    @Param('id', ParseIntPipe) bookmarkId: number,
+  ) {
+    return this.bookmarkService.deleteBookmarkById(userId, bookmarkId);
+  }
+}
